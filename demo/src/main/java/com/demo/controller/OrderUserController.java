710d555fc0debc2c5fb10e55799de789774a9251
BAOUCHbaSalma <baouch.salma0@gmail.com>
2024-08-31 21:34:00 +0100

    j'ai fait des modifications au niveau du controller du order

--- demo/src/main/java/com/demo/controller/OrderUserController.java
+++ demo/src/main/java/com/demo/controller/OrderUserController.java
@@ -1,5 +1,6 @@
 package com.demo.controller;
 
+import com.demo.model.OrderKey;
 import com.demo.model.OrderUser;
 import com.demo.service.OrderUserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +20,37 @@ public class OrderUserController {
     public OrderUser passOrder(@RequestBody OrderUser orderUser){
         return orderUserService.passeCommande(orderUser);
     }
+
+    @GetMapping("merchant/orders/{id}")
+    public OrderUser validationCommande(@PathVariable OrderKey id){
+        return orderUserService.validationCommande(id);
+    }
+
+    @GetMapping("merin/orders/{id}")
+    public OrderUser cancelCommande(@PathVariable OrderKey id){
+        return orderUserService.cancelCommande(id);
+    }
+
+    @GetMapping("admin/orders")
+    public List<OrderUser> findAllOrders(){
+        return orderUserService.findAll();
+    }
+
+    @GetMapping("admin/orders/pending")
+    public List<OrderUser> findOrdersPending(){
+        return orderUserService.findOrderPending();
+    }
+
+    @GetMapping("admin/orders/validate")
+    public List<OrderUser> findOrdersValidate(){
+        return orderUserService.findOrderValidate();
+    }
+
+    @GetMapping("admin/orders/cancel")
+    public List<OrderUser> findOrdersCancel(){
+        return orderUserService.findOrderCancel();
+    }
+
 
 
 
